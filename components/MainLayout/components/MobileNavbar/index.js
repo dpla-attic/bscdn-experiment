af2d0f88a8f3cc8363e538a8d45b1dbecd9ea9db
kcontreras01 <kcontreras01@gmail.com>
2020-12-22 13:38:24 -0500

    breakpoint for mobile nav working

--- components/MainLayout/components/MobileNavbar/index.js
+++ components/MainLayout/components/MobileNavbar/index.js
@@ -1,94 +1,58 @@
-import React, { Component } from 'react'
-import Link from 'next/link'
-import scss from "../MobileNavbar/MobileNavbar.module.scss"
-import MobileSearchBar from 'components/shared/MobileSearchBar'
+import { Drawer, Button, List, ListItem, ListItemText } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { makeStyles } from '@material-ui/core/styles';
 
-class MobileNavbar extends Component {
-  constructor() {
-    super()
+const useStyles = makeStyles(theme => ({
+  root: {
+    [theme.breakpoints.up(810)]: {
+      display: 'none'
+    },
+  },
+  list: {
+    width: 250,
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'flex-end'
+  }
+}));
 
-    this.state = {
-      menuDisplayed: false,
-      menuIcon: "/static/dpla-icons/menu-black.svg",
-      menuAlt: 'Open Menu'
+export default function MobileNavbar() {
+  const classes = useStyles();
+  const [menuDisplayed, setMenuDisplayed] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
     }
-  }
 
-  showMenu = () => {
-    this.setState(prevState => ({
-      menuDisplayed: !prevState.menuDisplayed
-    }))
-  }
+    setMenuDisplayed(open);
+  };
 
-  getMenuIcon = () => {
-    return this.state.menuDisplayed ? "/static/dpla-icons/close.svg" : "/static/mobile/icon/menu/menu-mobile.png"
-  }
+  const navItems = [
+    { text: 'Home', href: "/" },
+    { text: 'Search', href: "/search" },
+    { text: 'Visit DPLA', href: "https://dp.la/" },
+  ]
 
-  getMenuAlt = () => {
-    return this.state.menuDisplayed ? "Close Menu" : "Open Menu"
-  }
+  return (
+    <nav className={classes.root}>
+      <div className={classes.container}>
+        <Button onClick={toggleDrawer(true)}>
+          {menuDisplayed ? <CloseIcon /> : <MenuIcon />}
+        </Button>
+      </div>
 
-  render() {
-    const menuIcon = this.getMenuIcon();
-    const menuAlt = this.getMenuAlt()
-
-    return (
-      <nav className={scss.navbar} data-cy="navbar__mobile">
-        {/* logo and mobile menu icon*/}
-        <div className={scss.navbar__graphic}data-cy="nav__logo_mobile">
-          <Link href="/">
-            <a>
-              <img src="/static/mobile/logo/dpla_bws-logo-color-nav-mobile.png" alt="Black Women's Suffrage Logo"/>
-            </a>
-          </Link>
-
-          <button onClick={this.showMenu} name="menuDisplayed" data-cy="menuDisplayed">
-            <img src={menuIcon} id={scss.menuIcon} alt={menuAlt}/>
-          </button>
-        </div>
-
-        {/* nav links */}
-        {this.state.menuDisplayed &&
-          <div>
-            <MobileSearchBar />
-            <div className={scss.navbar__links} data-cy="nav__links_mobile">
-              <Link href="/about">
-                <a>About</a>
-              </Link>
-
-              <Link href="/timeline">
-                <a>Timeline</a>
-              </Link>
-
-              <Link href="/key-figures">
-                <a>Key Figures</a>
-              </Link>
-
-              <Link href="/collections">
-                <a>Collections</a>
-              </Link>
-
-              <Link href="/partners">
-                <a>Partners</a>
-              </Link>
-
-
-              <div className={scss.navbar__links_secondary}>
-                <div className={scss.navbar__links_divider}></div>
-                <a href="https://dp.la" target="_blank" rel="noopener">Visit DPLA</a>
-                <Link href="/harmful-language-statement">
-                  <a>Harmful Language Statement</a>
-                </Link>
-              </div>
-
-            </div>
-          </div>
-        }
-      </nav>
-    )
-  }
+      <Drawer anchor='right' open={menuDisplayed} onClose={toggleDrawer(false)}>
+        <List className={classes.list}>
+          {navItems.map((item, index) => (
+            <ListItem button component="a" href={item.href} key={`mobile-nav-item-${index}`}>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </nav>
+  );
 }
-
-
-
-export default MobileNavbar
--- components/MainLayout/index.js
+++ components/MainLayout/index.js
@@ -1,12 +1,14 @@
 import Helmet from "react-helmet"
 import GoogleAnalytics from "components/shared/GoogleAnalytics";
 import Navbar from "./components/Navbar";
+import MobileNavbar from "./components/MobileNavbar";
 
 const MainLayout = ({ children }) => {
   return (
     <GoogleAnalytics>
       <Helmet htmlAttributes={{ lang: "en" }} />
       <Navbar />
+      <MobileNavbar />
       {children}
     </GoogleAnalytics>
   )
