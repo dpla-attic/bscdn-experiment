66078e97903f3cba956a6d31465affec6381e3ca
kcontreras01 <kcontreras01@gmail.com>
2021-01-05 10:09:03 -0500

    fixed double searchbar bug

--- components/MainLayout/components/Navbar/index.js
+++ components/MainLayout/components/Navbar/index.js
@@ -1,19 +1,62 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
-import scss from "./Navbar.module.scss"
-import SearchBar from "components/shared/SearchBar";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchBar from "components/shared/SearchBar"
+import SearchIcon from '@material-ui/icons/Search'
 import { useRouter } from 'next/router'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  navbar: {
+    display: 'flex',
+    padding: '.5rem 1rem',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    [theme.breakpoints.down(810)]: {
+      display: 'none'
+    },
+    '& div': {
+      display: 'inline-flex'
+    },
+    '& a': {
+      textDecoration: 'none',
+      color: '#282830',
+      fontFamily: '$inter',
+      fontSize: '16px',
+      fontWight: '500',
+      letterSpacing: '0.2px',
+      marginRight: '1.75rem',
+      lineHeight: '35px',
+      justifyContent: 'center',
+      alignItems: 'center',
+      display: 'flex',
+      '& img': {
+        height: '50px'
+      }
+    }
+  },
+  searchbar: {
+    [theme.breakpoints.down(810)]: {
+      display: 'none'
+    }
+  },
+  divider: {
+    height: '35px',
+    width: '1px',
+    backgroundColor: '#D8D8D8',
+    marginRight: '1.75rem',
+  }
+}))
 
 export default function Navbar() {
   const router = useRouter()
   const [show, setShow] = useState(router.pathname === '/search' ? true : false)
   const toggle = () => setShow(!show)
+  const classes = useStyles()
 
   return (
     <>
-      <nav className={scss.navbar} data-cy="navbar">
-        <div className={scss.nav__logo} data-cy="nav__logo">
+      <nav className={classes.navbar} data-cy="navbar">
+        <div data-cy="nav__logo">
           <Link href="/">
             <a>
               <img src={'/static/logo/logo.png'} alt="Big Sky Digital Network" />
@@ -21,9 +64,9 @@ export default function Navbar() {
           </Link>
         </div>
 
-        <div className={scss.nav__links} data-cy="nav__links">
+        <div data-cy="nav__links">
           <a href="https://dp.la" target="_blank">Visit DPLA</a>
-          <div className={scss.divider} />
+          <div className={classes.divider} />
 
           <button onClick={toggle} data-cy="searchbar__icon">
             <SearchIcon />
@@ -31,64 +74,9 @@ export default function Navbar() {
         </div>
       </nav>
 
-      {/* {show && <SearchBar />} */}
+      <div className={classes.searchbar}>
+        {show && <SearchBar />}
+      </div>
     </>
   )
 }
-
-
-// class Navbar extends Component {
-//   constructor() {
-//     super()
-
-//     this.state = {
-//       showSearchbar: false
-//     }
-//   }
-
-//   componentDidMount = () => {
-//     let path = window.location.pathname
-//     if (path === '/search') {
-//       this.setState({
-//         showSearchbar: true
-//       })
-//     }
-//   }
-
-//   triggerSearchbar = () => {
-//     this.setState({
-//       showSearchbar: !this.state.showSearchbar
-//     })
-//   }
-
-//   render() {
-//     return (
-      // <>
-      //   <nav className={scss.navbar} data-cy="navbar">
-      //     <div className={scss.nav__logo} data-cy="nav__logo">
-      //       <Link href="/">
-      //         <a>              
-      //           <img src={'/static/logo/logo.png'} alt="Big Sky Digital Network"/>
-      //         </a>
-      //       </Link>
-      //     </div>
-
-      //     <div className={scss.nav__links} data-cy="nav__links">
-      //       <a href="https://dp.la" target="_blank">Visit DPLA</a>
-      //       <div className={scss.divider} />
-
-      //       <button onClick={this.triggerSearchbar} data-cy="searchbar__icon">
-      //         <SearchIcon />
-      //       </button>
-      //     </div>
-      //   </nav>
-
-      //   {this.state.showSearchbar &&
-      //     <SearchBar />
-      //   }
-      // </>
-//     )
-//   }
-// }
-
-// export default Navbar
