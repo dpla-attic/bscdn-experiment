8ab85531e449518bfab991803accb64b496c9162
Michael Della Bitta <mdellabitta@gmail.com>
2021-01-13 15:58:40 -0500

    sitemap implementation for item pages

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,4 +1,6 @@
-const absoluteLink = (path) => { return `https://${process.env.SITE_NAME}.dp.la${path}` }
+const readline = require('readline');
+const fs = require('fs');
+
 
 const buildEntry = (link, date) => {
     return `<url>
@@ -7,27 +9,38 @@ const buildEntry = (link, date) => {
 </url>
 `;
 }
-const sitemap = (req, res) => {
+const sitemap = async (req, res) => {
 
     const date = new Date().toISOString();
+    const baseUrl = `https://${process.env.SITE_NAME}.dp.la/item/`;
 
-    const staticLinks = [
-        '/'
-    ].map(absoluteLink);
+    const readInterface = readline.createInterface(
+        fs.createReadStream("public/bscdn_ids.csv")
+    );
 
-    const entries = []
-        .concat(staticLinks)
-        .map((link) => buildEntry(link, date));
-
-    const sitemap =
-        `<?xml version="1.0" encoding="UTF-8"?>\n` +
-        `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`
-        + "".concat(...entries)
-        + `\n</urlset>`;
+    const sitemapHeader =
+        '<?xml version="1.0" encoding="UTF-8"?>\n' +
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n' +
+        ' xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n';
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/xml');
-    res.end(sitemap);
+    res.write(sitemapHeader)
+
+    if (process.env.FULL_FRAME_IMAGES) {
+        for await (const line of readInterface) {
+            const itemId = JSON.parse(line);
+            const entry = `<url><loc>${baseUrl}${itemId}</loc><image:image><image:loc>https://bscdn-images.dp.la/${itemId}.jpg</image:loc></image:image><lastmod>${date}</lastmod></url>`;
+            res.write(entry);
+        }
+    } else {
+        for await (const line of readInterface) {
+            const itemId = JSON.parse(line);
+            const entry = `<url><loc>${baseUrl}${itemId}</loc><lastmod>${date}</lastmod></url>`;
+            res.write(entry);
+        }
+    }
+    res.end("</urlset>");
 };
 
 export default sitemap;
