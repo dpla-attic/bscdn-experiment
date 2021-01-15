4917917688f1f8d4be003fbf0bf48e5265848ee1
kcontreras01 <kcontreras01@gmail.com>
2021-01-15 08:59:12 -0500

    update itemimage component with modern react

--- components/ItemComponents/Content/ItemImage/index.js
+++ components/ItemComponents/Content/ItemImage/index.js
@@ -1,40 +1,54 @@
-import { getDefaultThumbnail } from "lib";
+import { getDefaultThumbnail } from "lib"
 
-import css from "./ItemImage.module.scss";
+import { useState, useEffect } from "react"
+import { makeStyles } from '@material-ui/core'
 
-class ItemImage extends React.Component {
-  state = {
-    updateToDefaultImage: false
-  };
-
-  componentDidMount() {
-    // Check for images that error so we can replace them with a default image
-    const _img = document.createElement("img");
-    _img.src = this.props.url;
-    _img.onerror = () => {
-      this.setState({ updateToDefaultImage: true });
-    };
+const useStyles = makeStyles(() => ({
+  imageWrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: '0.75rem',
+  },
+  image: {
+    borderRadius: '4px',
+    height: 'auto',
+  }, 
+  image_stretch: {
+    borderRadius: '4px',
+    width: '100%',
+    height: 'auto',
   }
+}))
 
-  render() {
-    const { title, type, url, useDefaultImage, defaultImageClass } = this.props;
-    const { updateToDefaultImage } = this.state;
-    const useDefaultWrapper = updateToDefaultImage || useDefaultImage;
+const ItemImage = ({ title, type, url, useDefaultImage, defaultImageClass }) => {
+  const [updateToDefaultImage, setUpdateToDefaultImage] = useState(false)
+  const useDefaultWrapper = updateToDefaultImage || useDefaultImage
+  const classes = useStyles()
+
+  useEffect(() => {
+     // Check for images that error so we can replace them with a default image
+     const _img = document.createElement("img")
+     _img.src = url
+     _img.onerror = () => {
+       setUpdateToDefaultImage(true)
+     }
+  }, [])
+
     return (
       <div
-        className={`${css.imageWrapper}
+        className={`${classes.imageWrapper}
           ${useDefaultWrapper
-            ? css.defaultImageWrapper + " " + defaultImageClass
+            ? classes.defaultImageWrapper + " " + defaultImageClass
             : ""}`}
       >
         <img
           src={updateToDefaultImage ? getDefaultThumbnail(type) : url}
           alt={title}
-          className={ process.env.FULL_FRAME_IMAGES ? css.image_stretch : css.image }
+          className={ process.env.FULL_FRAME_IMAGES ? classes.image_stretch : classes.image }
         />
       </div>
-    );
-  }
+    )
+
 }
 
-export default ItemImage;
+export default ItemImage
