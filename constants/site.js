31c9b1956cbea8ffc7a3f6b52b1688b724fcec91
Michael Della Bitta <mdellabitta@gmail.com>
2021-06-22 16:27:15 -0400

    site content is no longer determined by a tag but by a separate api

--- constants/site.js
+++ constants/site.js
@@ -1,5 +1,5 @@
 export const GA_TRACKING_ID = "UA-28197764-11";
-export const SITE_TAG = "tags:bscdn-experiment";
+export const SITE_TAG = "" //"tags:bscdn-experiment";
 
 export const UNTITLED_TEXT = "Untitled";
 
